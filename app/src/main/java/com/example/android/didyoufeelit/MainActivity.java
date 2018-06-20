0514d0a360045d991125654a2f33db8c21443c24
Hudson Pereira <hudson.byte@gmail.com>
2018-06-20 09:39:43 -0300

    moving network operations to background using async task

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.didyoufeelit;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -34,11 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Perform the HTTP request for earthquake data and process the response.
-        Event earthquake = Utils.fetchEarthquakeData(USGS_REQUEST_URL);
-
-        // Update the information displayed to the user.
-        updateUi(earthquake);
+        new EarthquakeDataAsyncTask().execute(USGS_REQUEST_URL);
     }
 
     /**
@@ -53,5 +50,17 @@ public class MainActivity extends AppCompatActivity {
 
         TextView magnitudeTextView = (TextView) findViewById(R.id.perceived_magnitude);
         magnitudeTextView.setText(earthquake.perceivedStrength);
+    }
+
+    class EarthquakeDataAsyncTask extends AsyncTask<String, Void, Event> {
+        @Override
+        protected Event doInBackground(String... strings) {
+            return Utils.fetchEarthquakeData(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Event event) {
+            updateUi(event);
+        }
     }
 }
